a5e27b46172d310d7111aef818050edfb8c9dabd
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2024-11-25 14:53:27 +0100

    Oppdatert størrelse på skeletonkomponent

--- src/components/loader/skeleton/Skeleton.tsx
+++ src/components/loader/skeleton/Skeleton.tsx
@@ -4,10 +4,10 @@ import styles from "./Skeletone.module.css"
 const SkeletonComponent = () => {
   return (
     <div className={styles.container}>
-      <Skeleton variant="text" width="70%" height="5rem" />
-      <Skeleton variant="text" width="100%" height="4.25rem" />
-      <Skeleton variant="text" width="100%" height="4.25rem" />
-      <Skeleton variant="text" width="100%" height="4.25rem" />
+      <Skeleton variant="text" width="60%" height="5rem" />
+      <Skeleton variant="text" width="100%" height="3.5rem" />
+      <Skeleton variant="text" width="100%" height="3.5rem" />
+      <Skeleton variant="text" width="100%" height="3.5rem" />
     </div>
   );
 };
