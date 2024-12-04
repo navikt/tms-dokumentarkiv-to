9285bf66b434e0c651c58b593e6e7c8ad03dfdfd
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2024-12-04 11:14:28 +0100

    Fjerner ubrukte tekstvariabler og legger inn nn-oversettelser fra Ole Jonas

--- src/language/text.ts
+++ src/language/text.ts
@@ -1,12 +1,7 @@
 export const text = {
-  dokumentarkiv: {
-    nb: "Dokumentarkiv",
-    nn: "Dokumentarkiv",
-    en: "Document archive",
-  },
   dokumenter: {
     nb: "Dokumenter",
-    nn: "Dokumenter",
+    nn: "Dokument",
     en: "Documents",
   },
   minSide: {
@@ -21,77 +16,32 @@ export const text = {
   },
   viserAntallDokumenter: {
     nb: (synlige: number, antall: number ) => `Viser ${synlige} av ${antall} dokumenter`,
-    nn: (synlige: number, antall: number ) => `Viser ${synlige} av ${antall} dokumenter`,
+    nn: (synlige: number, antall: number ) => `Viser ${synlige} av ${antall} dokument`,
     en: (synlige: number, antall: number ) => `Showing ${synlige} of ${antall} documents`,
   },
-  dokumentArkivTittel: {
-    nb: "Dokumentarkiv",
-    nn: "Dokumentarkiv",
-    en: "Document archive",
-  },
-  dokumentTittelEntall: {
-    nb: "Dokument",
-    nn: "Dokument",
-    en: "Document",
-  },
-  seAlleDokumenter: {
-    nb: "Se alle dokumenter",
-    nn: "Sjå alle dokument",
-    en: "See all documents",
-  },
-  dokumentListeTittel: {
-    nb: "Dokumenter",
-    nn: "Dokumenter",
-    en: "Documents",
-  },
-  dokumentArkivIngress: {
-    nb: "Her finner du alle journalførte dokumenter relatert til",
-    nn: "Her finn du alle journalførte dokument knytt til",
-    en: "Here you will find all archived documents related to",
-  },
-  sendtAv: {
-    nb: (avsender: string) => `Fra ${avsender}`,
-    nn: (avsender: string) => `Fra ${avsender}`,
-    en: (avsender: string) => `From ${avsender}`,
-  },
-  sendtTil: {
-    nb: (avsender: string) => `Til ${avsender}`,
-    nn: (avsender: string) => `Til ${avsender}`,
-    en: (avsender: string) => `Too ${avsender}`,
-  },
   lasterInn: {
-    nb: "Tredjepart",
-    nn: "Tredjepart",
-    en: "Third party",
+    nb: "Laster...",
+    nn: "Laster...",
+    en: "Loading",
   },
   representasjonLabel: {
     nb: "Hvem vil du bruke dokumentarkivet på vegne av?",
     nn: "Kven vil du bruke dokumentarkivet på vegner av?",
     en: "Which users documents would you like to view?",
   },
-  representasjonDescription: {
-    nb: "Velg mellom dine representerte for å se dokumentarkivet deres.",
-    nn: "Velg mellom dine representerte for å se dokumentarkivet deres.",
-    en: "Choose between the people you represent to view their documents.",
-  },
   representasjonStandardTekst: {
     nb: "Alle dokumenter som er registrert på ",
-    nn: "Alle dokumenter som er registrert på ",
+    nn: "Alle dokument som er registrert på ",
     en: "All documents registered for ",
-  },
-  representasjonValgtBruker: {
-    nb: "Du bruker nå dokumentarkivet på vegne av ",
-    nn: "Du brukar no dokumentarkivet på vegner av ",
-    en: "You are now viewing the document archive on behalf of ",
   },
   sosialhjelpTekst: {
     nb: "Dokumenter om ",
-    nn: "Dokumenter om ",
+    nn: "Dokument om ",
     en: "Documents regarding ",
   },
   sosialhjelpLenketekst: {
     nb: "økonomisk sosialhjelp finner du her.",
-    nn: "økonomisk sosialhjelp finner du her.",
+    nn: "økonomisk sosialhjelp finn du her.",
     en: "økonomisk sosialhjelp can be found on this page.",
   },
   representasjonDeg: {
@@ -104,105 +54,15 @@ export const text = {
     nn: "Sjå digitale fullmakter",
     en: "See your digital powers of attorney",
   },
-  snarveier: {
-    nb: "Snarveier",
-    nn: "Snarveier",
-    en: "Shortcuts",
-  },
-  saksbehandlingstider: {
-    nb: "Saksbehandlingstider",
-    nn: "Saksbehandlingstider",
-    en: "Processing times",
-  },
-  sendKlage: {
-    nb: "Send klage på vedtak",
-    nn: "Send klage på vedtak",
-    en: "Send an appeal against the decision",
-  },
-  dokumentDisclaimerTittel: {
-    nb: "Dokumenter du ikke får åpnet",
-    nn: "Dokumenter du ikke får åpnet",
-    en: "Not all types of documents are able to be viewed",
-  },
-  documentDisclaimerListepunktEn: {
-    nb: "Papirer du har sendt til NAV i posten. Du vil kunne se tittelen og dato for dokumentet, men ikke innholdet i pdf. Dokumenter du har sendt i papirpost blir skannet og digitalisert. Det er en liten risiko for at det skjer noe feil i skanningen. Vi vil ikke vise deg dokumentet før vi er sikre på at det gjelder deg og din sak",
-    nn: "Papirer du har sendt til NAV i posten. Du vil kunne se tittelen og dato for dokumentet, men ikke innholdet i pdf. Dokumenter du har sendt i papirpost blir skannet og digitalisert. Det er en liten risiko for at det skjer noe feil i skanningen. Vi vil ikke vise deg dokumentet før vi er sikre på at det gjelder deg og din sak",
-    en: "Documents sent to NAV by mail",
-  },
-  documentDisclaimerListepunktTo: {
-    nb: "Dokumenter som gjelder saken din, men som er sendt av andre på vegne av deg.  Du vil kunne se tittelen og dato for dokumentet, men ikke innholdet i pdf. Slike dokumenter kan for eksempel være en lege, advokat, verge eller fullmektig.",
-    nn: "Dokumenter som gjelder saken din, men som er sendt av andre på vegne av deg.  Du vil kunne se tittelen og dato for dokumentet, men ikke innholdet i pdf. Slike dokumenter kan for eksempel være en lege, advokat, verge eller fullmektig.",
-    en: "Documents regarding your case, sent by others on your behalf. This could mean that it was sent by a doctor, laywer or legal guardian.",
-  },
   landingssideDisclaimerTittel: {
     nb: "Finner du ikke det du leter etter?",
     nn: "Finn du ikkje det du leitar etter?",
     en: "Do you not find what you were looking for?",
   },
-  landingssideDisclaimerListepunktEn: {
-    nb: "Her viser vi alle dokumenter som er registrert på <innlogget navn/fullmakt>",
-    nn: "Her viser vi alle dokumenter som er registrert på <innlogget navn/fullmakt>",
-    en: "If you have sent an application by mail it will take some time before it is shown here",
-  },
   landingssideDisclaimerListepunktTo: {
     nb: "Vi viser dokumenter i saker nyere enn 2016. For bidragssaker kan du kun se dokumenter fra starten av 2022.",
-    nn: "Vi viser dokumenter i saker nyere enn 2016. For bidragssaker kan du kun se dokumenter fra starten av 2022.",
+    nn: "Vi viser dokument i sakar nyare enn 2016. For bidragssakar kan du kun sjå dokument frå starten av 2022.",
     en: "For contribution matters, we are only able to present documents starting from 2022.",
-  },
-  landingssideDisclaimerListepunktTre: {
-    nb: "Har du søkt om økonomisk sosialhjelp? Da er dokumenter knyttet til digitale søknader tilgjengelige i en ",
-    nn: "Har du søkt om økonomisk sosialhjelp? Da er dokumenter knyttet til digitale søknader tilgjengelige i en ",
-    en: "For contribution matters, we are only able to present documents starting from 2022.",
-  },
-  landingssideDisclaimerListepunktTreLenke: {
-    nb: "egen løsning",
-    nn: "egen løsning",
-    en: "For contribution matters, we are only able to present documents starting from 2022.",
-  },
-  landingssideDisclaimerTekstDel1: {
-    nb: "Kontakt oss",
-    nn: "Kontakt oss",
-    en: "Contact us",
-  },
-  landingssideDisclaimerTekstDel2: {
-    nb: " dersom det er noe du lurer på.",
-    nn: " dersom det er noko du lurar på.",
-    en: " if you have any questions.",
-  },
-  ingenSakerTittel: {
-    nb: "Ingen saker eller dokumenter å vise",
-    nn: "Ingen saker eller dokument å vise",
-    en: "No cases or documents to show",
-  },
-  ingenSakerListeTittel: {
-    nb: "Finner du ikke det du leter etter?",
-    nn: "Finner du ikke det du leter etter?",
-    en: "Can't find what you're looking for?",
-  },
-  ingenSakerListe1: {
-    nb: "Har du sendt inn en søknad per post tar det litt tid før saken vises her. Du kan likevel ettersende dokumenter.",
-    nn: "Har du sendt inn ein søknad per post tar det litt tid før saka blir vist her. Du kan likevel ettersende dokument.",
-    en: "If you have sent an application by mail it will take some time before it is shown on this page. You can still send additional documentation.",
-  },
-  ingenSakerListe3: {
-    nb: "For bidragssaker kan du kun se dokumenter fra starten av 2022.",
-    nn: "For bidragssaker kan du kun se dokumenter fra starten av 2022.",
-    en: "For contribution matters, we are only able to present documents starting from 2022.",
-  },
-  ingenSakerTaKontakt: {
-    nb: "Ta kontakt dersom det er noe du lurer på.",
-    nn: "Ta kontakt dersom det er noe du lurer på.",
-    en: "Contact us if you have any questions.",
-  },
-  minSideKnapp: {
-    nb: "Gå til Min Side",
-    nn: "Gå til Mi Side",
-    en: "Go to My Page",
-  },
-  tilbakeKnapp: {
-    nb: "Tilbake til forside dokumentarkiv",
-    nn: "Tilbake til forside dokumentarkiv",
-    en: "Back to document archive front page",
   },
   ingenJournalposter: {
     nb: "Ingen registrerte saker eller dokumenter å vise",
@@ -213,11 +73,6 @@ export const text = {
     nb: "Kunne ikke finne dokument",
     nn: "Kunne ikkje finne dokument",
     en: "Could not find document",
-  },
-  kanIkkeViseDokument: {
-    nb: "Kan ikke vise dokument",
-    nn: "Kan ikkje vise dokument",
-    en: "Can not show document",
   },
   nyttigOgVite: {
     nb: "Nyttig å vite",
@@ -246,7 +101,7 @@ export const text = {
   },
   temaLenkeDel2: {
     nb: " finner du informasjon om endringer og status i saken din.",
-    nn: " finner du informasjon om endringer og status i saken din.",
+    nn: " kan du finne informasjon om endringar og status i saken din.",
     en: " you will find information about changes and status on your case.",
   },
   sykOgSymLenke: {
@@ -269,11 +124,6 @@ export const text = {
     nn: "Skjul vedlegg",
     en: "Hide attachments",
   },
-  dokumentKanIkkeVises: {
-    nb: "Dokumentet kan ikke vises",
-    nn: "Kan ikkje vise dokument",
-    en: "Document can not be viewed",
-  },
   vedleggKanIkkeVises: {
     nb: " (kan ikke vises)",
     nn: " (kan ikkje visast)",
@@ -281,12 +131,12 @@ export const text = {
   },
   detaljerTitle: {
     nb: "Detaljer",
-    nn: "Detaljer",
+    nn: "Detaljar",
     en: "Details",
   },
   datoTitle: {
     nb: "Dato opprettet",
-    nn: "Dato opprettet",
+    nn: "Dato oppretta",
     en: "Date created",
   },
   temaTitle: {
@@ -304,34 +154,29 @@ export const text = {
     nn: "Sendt til",
     en: "Sent too",
   },
-  ingenSide404: {
-    nb: "Feil: Fant ikke siden",
-    nn: "Feil: Fant ikke siden",
-    en: "Error: Page not found",
-  },
   feilmelding: {
     nb: "Vi har for øyeblikket tekniske problemer. Dette kan føre til at du ikke får opp all informasjon. Vennligst prøv igjen senere.",
     en: "We're having technical difficulties. Some information may not be available, please try again later.",
-    nn: "Vi har for øyeblikket tekniske problemer. Dette kan føre til at du ikke får opp all informasjon. Vennligst prøv igjen senere.",
+    nn: "Vi har tekniske problem akkurat no. Dette kan føre til at du ikkje får opp all informasjon. Ver venleg og prøv igjen seinare.",
   },
   filtersTitle: {
     nb: "Velg hvilke dokumenter du vil se",
-    nn: "Velg hvilke dokumenter du vil se",
+    nn: "Vel dokumenta du vil sjå",
     en: "Choose which category of documents you wish to see",
   },
   tilgangssperreSkannet: {
     nb: "Du får ikke åpnet dokumenter du har sendt i papirpost. ",
-    nn: "Du får ikke åpnet dokumenter du har sendt i papirpost. ",
+    nn: "Du får ikkje opna dokument du har sendt i papirpost. ",
     en: "You can not open documents sendt by mail. ",
   },
   tilgangssperreTredjepart: {
     nb: "Du får ikke åpnet dette dokumentet fordi det er sendt av noen andre enn deg. ",
-    nn: "Du får ikke åpnet dette dokumentet fordi det er sendt av noen andre enn deg. ",
+    nn: "Du får ikkje opna dette dokumentet fordi det er sendt av nokon andre enn deg. ",
     en: "You can not open this document because it sent by someone other than yourself. ",
   },
   tilgangssperreAnnet: {
     nb: "Du får ikke åpnet dette dokumentet. ",
-    nn: "Du får ikke åpnet dette dokumentet. ",
+    nn: "Du får ikkje opna dette dokumentet. ",
     en: "You can not open this document. ",
   },
 };
