88a2ac7a1d517c9b7f128ab8b0f7114c3fe91bc2
Eivinn Eilertsen <eivinne@MacBookPro.home>
2026-02-13 10:10:11 +0100

    Rettet feil funnet i UU gjennomgang

--- src/components/representasjon/SelectFullmakt.tsx
+++ src/components/representasjon/SelectFullmakt.tsx
@@ -1,7 +1,7 @@
 import type {JournalpostProps} from "@components/journalpostliste/JournalpostInterfaces";
 import type {Language} from "@language/language";
 import {text} from "@language/text";
-import {BodyShort, Select} from "@navikt/ds-react";
+import {BodyShort, Heading, Select} from "@navikt/ds-react";
 import {
   getAlleJournalposterUrl,
   getFullmaktForhold,
@@ -168,7 +168,12 @@ const SelectFullmakt = ({language}: {language: Language}) => {
         </div>
       ) : null}
       {fullmaktInfo?.viserRepresentertesData && (
-        <BodyShort size="medium" className={styles.heading} aria-live="polite">
+        <Heading
+          className={styles.representasjonsHeading}
+          level="2"
+          size="xsmall"
+          aria-live="polite"
+        >
           {text.representasjonStandardTekst[language] +
             fullmaktInfo?.representertNavn +
             ". "}
@@ -182,7 +187,7 @@ const SelectFullmakt = ({language}: {language: Language}) => {
               </a>
             </span>
           )}
-        </BodyShort>
+        </Heading>
       )}
     </>
   );
