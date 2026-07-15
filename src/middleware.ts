385ae8a9ba730a52a9c30f1c2a3b519a7eda009c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-07-15 16:32:48 +0200

    Formattering

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export const onRequest = sequence(
   authenticate(),
   defineMiddleware(async (context, next) => {
     const localeRedirect = defaultLocaleRedirect(context);
+
     if (localeRedirect) {
       return context.redirect(localeRedirect);
     }
